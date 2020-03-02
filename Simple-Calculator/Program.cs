2b816d74938c990ec15d75fbfae3c38394439b54
kellycoles <kellycoles1@gmail.com>
2020-03-01 20:31:07 -0600

    calculate method

--- Simple-Calculator/Program.cs
+++ Simple-Calculator/Program.cs
@@ -6,7 +6,7 @@ namespace Simple_Calculator
     {
         static void Main(string[] args)
         {
-            PrintMenu();
+          
         }
         public static void PrintMenu()
         {
@@ -18,6 +18,37 @@ namespace Simple_Calculator
             Console.WriteLine("3. Multiplication");
             Console.WriteLine("4. Division");
             Console.WriteLine();
+        }
+
+        public static void Calculate(int SelectedOption)
+        {
+            int Result = 0;
+            Console.WriteLine("Enter your first number");
+            int FirstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your second number");
+            int SecondNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+
+            switch (SelectedOption)
+            {
+                case 1:
+                    Result = FirstNumber + SecondNumber;
+                    Console.WriteLine("{0} + {1} = {2}", FirstNumber, SecondNumber, Result);
+                    break;
+                case 2:
+                    Result = FirstNumber - SecondNumber;
+                    Console.WriteLine("{0} - {1} = {2}", FirstNumber, SecondNumber, Result);
+                    break;
+                case 3:
+                    Result = FirstNumber * SecondNumber;
+                    Console.WriteLine("{0} * {1} = {2}", FirstNumber, SecondNumber, Result);
+                    break;
+                case 4:
+                    Result = FirstNumber / SecondNumber;
+                    Console.WriteLine("{0} / {1} = {2}", FirstNumber, SecondNumber, Result);
+                    break;
+            }
         }
     }
 
