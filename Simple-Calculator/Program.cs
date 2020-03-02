5b2e388ad7150d930ffca732ad23112303db9990
kellycoles <kellycoles1@gmail.com>
2020-03-01 20:04:10 -0600

    created print menu

--- Simple-Calculator/Program.cs
+++ Simple-Calculator/Program.cs
@@ -6,7 +6,19 @@ namespace Simple_Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            PrintMenu();
+        }
+        public static void PrintMenu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Select your option");
+            Console.WriteLine("------------------");
+            Console.WriteLine("1. Addition");
+            Console.WriteLine("2. Subtraction");
+            Console.WriteLine("3. Multiplication");
+            Console.WriteLine("4. Division");
+            Console.WriteLine();
         }
     }
+
 }
